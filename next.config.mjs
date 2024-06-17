df09ab3c76bfccfbe2eab66bfb4032658ad312a8
Jose Salcido <jmsalcidoaguilar@gmail.com>
2024-06-16 21:01:15 -0700

    webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = resolve(__dirname, 'src');
+    return config;
+  },
   images: {
     domains: ['res.cloudinary.com'], // Add the new host here
     remotePatterns: [
