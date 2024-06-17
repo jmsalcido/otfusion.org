d0ceaa5b806049ef8895de6059d8dc7258baf0c2
Jose Salcido <jmsalcidoaguilar@gmail.com>
2024-06-16 17:43:33 -0700

    identify all type of people in posthog

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -8,7 +8,7 @@ import { PostHogProvider } from 'posthog-js/react'
 if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: 'identified_only',
+    person_profiles: 'always',
   })
 }
 
