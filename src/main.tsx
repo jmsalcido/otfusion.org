bf37255925981fe5bf8a4e29d2cbd864b9977cd4
Jose Salcido <jmsalcidoaguilar@gmail.com>
2025-07-06 23:00:02 -0700

    fix posthog

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,13 @@ import './index.css'
 import App from './App.tsx'
 import { PostHogProvider } from 'posthog-js/react'
 
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}>
+    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
       <App />
     </PostHogProvider>
   </StrictMode>,
