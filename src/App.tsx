141bca0d2b38dfd7b563e55ca36edf32562aa295
Jose Salcido <jmsalcidoaguilar@gmail.com>
2025-07-06 22:19:19 -0700

    add the 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Projects } from './pages/Projects'
+import { NotFound } from './pages/NotFound'
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   )
