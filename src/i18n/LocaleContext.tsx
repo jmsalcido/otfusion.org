136b18483da802c330de81ff308c852f143a535e
Jose Salcido <jmsalcidoaguilar@gmail.com>
2025-11-17 22:21:54 -0700

    update lint

--- src/i18n/LocaleContext.tsx
+++ src/i18n/LocaleContext.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useMemo, useState, type ReactNode } from 'react'
 import { translations, type Locale, type Translation } from './translations'
 
